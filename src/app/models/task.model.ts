777d75ee2008853128a239811e15d1791e4e31da
KortexDev <cacortesl01@gmail.com>
2025-02-17 00:04:16 -0500

    agregando método de actualizar tareas

---
+++ src/app/models/task.model.ts
@@ -0,0 +1,7 @@
+interface TaskModel {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
+export default TaskModel;
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpEvent } from '@angular/common/http';
+import TaskModel from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +9,13 @@ import { HttpEvent } from '@angular/common/http';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  tasksList = signal([
-    { task: 'Ver seven scissor 4' },
-    { task: 'Aprender Angular' },
+  tasksList = signal<TaskModel[]>([
+    {
+      id: Date.now().toString(),
+      title: 'Ver seven scissor 4',
+      completed: false,
+    },
+    { id: Date.now().toString(), title: 'Aprender Angular', completed: false },
   ]);
   trackByFnTask(item: any): number {
     return item.task; // Usamos el ID único como clave
@@ -21,10 +25,34 @@ export class HomeComponent {
     if (event.key === 'Enter') {
       const input = event.target as HTMLInputElement;
       const value = input.value;
-      this.tasksList.update((currentList) => [...currentList, { task: value }]);
+      this.addTask(value);
     }
     return;
   }
+  //Método para agregar tareas
+  addTask(value: string) {
+    const newTask = {
+      id: Date.now().toString(),
+      title: value,
+      completed: false,
+    };
+    this.tasksList.update((currentList) => [...currentList, newTask]);
+  }
+  //Actualizar las tareas
+  updateTask(index: number) {
+    console.log('actualizando');
+    this.tasksList.update((currentList) => {
+      const newTaskList = currentList.map((task, taskIndex) => {
+        if (index === taskIndex) {
+          task.completed = !task.completed;
+        }
+        return task;
+      });
+      console.log(newTaskList);
+      return newTaskList;
+    });
+  }
+  //Método para borrar tareas
   deleteTask(index: number) {
     this.tasksList.update((currentList) => {
       return currentList.filter((_item, position) => index !== position);
